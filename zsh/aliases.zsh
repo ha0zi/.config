ed5722c2bf2dd589f22efaa14bae6f296a3e5836
ha0zi <249055274@qq.com>
2024-04-18 11:34:19 +0800

    update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 alias ls='ls --color=auto'
-alias l='ls -a --color=auto'
+alias l='ls --color=auto'
+alias la='ls -a --color=auto'
 alias ll='ls -al --color=auto'
 alias lh='ls -lh --color=auto'
 
@@ -7,15 +8,26 @@ alias rm='rm -i'
 
 alias vim='nvim'
 
+# quicks cd
+alias cdfrp='cd /home/hz/Dev/utils/frp_0.49.0_linux_amd64'
+
 # touchpad
 alias show-touid='xinput list | grep -i touchpad'
 alias kill-touid='xinput --disable '
 
 ## nas
-# mount nas-hz data_bak dir to /mnt
+# mount nas-hz data_bak dir to /mnt/data_bak/
 alias mountnas='bash /home/hz/.local/scripts/nas/mount-hz-nas-data'
+# mount nas-hz images_data dir to /mnt/images_data/
+alias mountni='bash /home/hz/.local/scripts/nas/mount-hz-images'
 # rsync notebook to nas-hz
-alias sync-note='bash /home/hz/.local/scripts/nas/'
+alias sync-note='bash /home/hz/.local/scripts/nas/sync-local-note-nas'
+# rsync calibre to nas-hz
+alias sync-calibre='bash /home/hz/.local/scripts/nas/sync-local-calibre-nas'
+
+## xrandr
+# ipad4
+alias xrandr-ipad4='bash ~/.local/scripts/other/xrandr-ipad4'
 
 # install apps quicks
 # joshuto
